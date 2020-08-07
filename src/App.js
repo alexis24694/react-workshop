b63bf8e77b7633d28fae5438183a1852d09c0118
Cesar Marchena <cesar.marchena@outlook.com>
2020-08-06 21:56:43 -0500

    Fixing errors

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Nav from './components/nav/Nav';
-import UserCreate from './components/user/UserCreate';
-import UserUpdate from './components/user/UserUpdate';
+import Nav from './components/Nav/Nav';
+import UserCreate from './components/User/UserCreate';
+import UserUpdate from './components/User/UserUpdate';
 import List from './components/userlist/List';
 import Catalogo from './components/catalogo/Catalogo';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
